227086dc717d3cf61558552b0f4cc35080dc6453
Dariusz Osiadacz <darekosiadacz@gmail.com>
2019-09-30 22:40:28 +0200

    first commit

---
+++ src/containers/Users.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Users extends Component {
+  render() {
+    return (
+      <div>
+        <h1>The Users</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Consectetur doloremque ea expedita, laborum molestias odit quod tempora veritatis!
+          A accusantium alias autem dignissimos doloremque optio quasi, ratione recusandae sed voluptates.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default Pizza;
